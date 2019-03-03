3f7c850b2938172d0ec2cbd91016e807708b21f0
Danny Wakeling <wakeling101@hotmail.com>
2019-03-03 20:17:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,10 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{|name, index| puts "#{index+1} #{name}"}
 end
 
-def summon_captain_planet(planet)
-  planet.collect {|x| x.capitalize + "!"}
-  return planet
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
